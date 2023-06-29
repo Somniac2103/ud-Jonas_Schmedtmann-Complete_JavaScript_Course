2d6168608a566e06d5007ad6507e456f2521bf6b
Somniac2103 <salmon.barry1@gmail.com>
2023-06-29 19:39:51 +0700

    Daily javascript training completed

---
+++ Section2-Fundamentals_Part1/exercises.js
@@ -0,0 +1,23 @@
+//VALUES AND VARIABLES
+
+/* 
+let country = "South Africa";
+let continent = "Africa";
+let population = 52;
+
+console.log(country);
+console.log(continent);
+console.log(population); 
+*/
+s
+//DATATYPES
+
+/* 
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(language); 
+*/
+
+//LET CONST VAR
---
+++ Section2-Fundamentals_Part1/script.js
@@ -0,0 +1,52 @@
+//VALUES AND VARIABLES
+/* 
+let js = 'amazing';
+console.log(40+8/23-10);
+
+console.log('Barry');
+console.log(23);
+
+let firstName = "Keith";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName); 
+*/
+
+//DATATYPES
+
+/* 
+true;
+console.log(true);
+let value = false;
+console.log(value);
+
+console.log(typeof true);
+console.log(typeof value);
+console.log(typeof 23);
+console.log(typeof "Barry");
+
+value = "Yes!"
+console.log(value);
+console.log(typeof value);
+
+let year;
+console.log(year)
+console.log(typeof year);
+
+year = 1991;
+console.log(year)
+console.log(typeof year);
+ */
+
+//LET CONST VAR
+
+/* 
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+var job = "programmer";
+job = "teacher"; 
+*/
